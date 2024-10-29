9b9cbb44e46ab4060763fd27e55d40f4cc1d4b83
Your Name <you@example.com>
2024-10-29 01:51:37 -0700

    update auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes: ['/', '/about', '/contact','/policy']
+    publicRoutes: ['/', '/about', '/contact','/policy', "/api/get-top-sellers", "/api/get-prompts"]
 });
  
 export const config = {
       matcher: ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
+    //   ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/get-top-sellers"  ]
 };
    
